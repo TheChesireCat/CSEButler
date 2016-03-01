df2c982fb00a24ad95f541d491b9239d0679ee06
TheChesireCat <aliceoxenbury@gmail.com>
2016-03-01 12:32:00 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var token = '181307459:AAE5vJaFSWKTxtCo33HkYgnRm5GSAb1gRvI'
 var Bot = require('node-telegram-bot-api'),
     bot;
 var BASE_URL = "https://csebutler.herokuapp.com/"
-
+var track={};
 var app= express();
 
 if(process.env.NODE_ENV ==='production'){
@@ -24,7 +24,19 @@ if(process.env.NODE_ENV ==='production'){
 console.log('bot server started...');
 
 bot.onText(/^\/invite.*$/,function(msg){
-  bot.sendMessage(msg.chat.id,'Hey '+msg.from.first_name+'! the invite link for the CSE supergroup is : https://telegram.me/joinchat/ACZESjxi8y7an1ZcAdpBpA').then(function(){});
+    if(track[msg.chat.id == undefined]){
+      bot.sendMessage(msg.chat.id,'Hey '+msg.from.first_name+'! the invite link for the CSE supergroup is : https://telegram.me/joinchat/ACZESjxi8y7an1ZcAdpBpA').then(function(){});
+      track[msg.chat.id]=0;
+  } else if (track[msg.chat.id]>0) {
+    bot.sendMessage(msg.chat.id,'Hey '+msg.from.first_name+'! you probably didn\'t see the link, please scroll up a bit.').then(function(){});
+  } else if(track[msg.chat.id]==0){
+    bot.sendMessage(msg.chat.id,'Hey '+msg.from.first_name+'! the invite link for the CSE supergroup is : https://telegram.me/joinchat/ACZESjxi8y7an1ZcAdpBpA').then(function(){});
+  }
+});
+
+bot.on('message',function(msg){
+  var chatId = msg.chat.id;
+  track[chatId] = (track[chatId]+1)%10;
 });
 
 var server = app.listen(process.env.PORT,function(){
